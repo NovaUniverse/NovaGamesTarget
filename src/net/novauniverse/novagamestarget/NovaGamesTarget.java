8f3deae673f92dd16109282d15a22bb25a4a8141
Anton udén <udenanton@hotmail.com>
2021-03-21 18:51:09 +0100

    Initial commit

---
+++ src/net/novauniverse/novagamestarget/NovaGamesTarget.java
@@ -0,0 +1,14 @@
+package net.novauniverse.novagamestarget;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class NovaGamesTarget extends JavaPlugin {
+	@Override
+	public void onEnable() {
+		getLogger().info("Rached target: NovaGamesTarget");
+	}
+
+	@Override
+	public void onDisable() {
+	}
+}
